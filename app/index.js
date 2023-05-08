4f4833dbe6f973e0f01a54784f64043acfe4e44d
Grampachampa <grampachampa@gmail.com>
2023-05-08 20:39:33 +0200

    Fuck staginge

--- app/index.js
+++ app/index.js
@@ -1,10 +1,59 @@
-import { View, Text } from 'react-native';
+import {useState} from 'react';
+import { ScrollView, SafeAreaView, Text, View } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+
+// FIX LATER
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components';
+
 
 const Home = () => {
+    const router = useRouter();
+    var horizontalPaddingHRight = 5;
+    var horizontalPaddingHLeft = 20;
+    var verticalPaddingHLeft = 0;
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style = {{fled: 1, backgroundColor: COLORS.lightWhite}}>
+            
+            <Stack.Screen 
+            options = {{
+                headerStyle: {backgroundColor: COLORS.backgroundBlue},
+                headerShadowVisible: false,
+
+                headerLeft: () => (
+                    <View style={{paddingHorizontal: horizontalPaddingHLeft, marginVertical:verticalPaddingHLeft}}><ScreenHeaderBtn iconUrl={icons.logo} dimension="150%" resize = "center" /></View>
+                ),
+                headerRight: () => (
+                    
+                    <View style={{flexDirection:"row"}}>
+                        <View style={{paddingHorizontal: horizontalPaddingHRight}}><ScreenHeaderBtn iconUrl={icons.tuning} dimension="100%"/></View>
+                        <View style={{paddingHorizontal: horizontalPaddingHRight}}><ScreenHeaderBtn iconUrl={icons.notification} dimension="80%"/></View>
+                        <View style={{paddingHorizontal: horizontalPaddingHRight}}><ScreenHeaderBtn iconUrl={icons.message} dimension="100%" resize = "cover" /></View>
+                    </View>
+                ),
+                headerTitle: "",
+            }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator = {true}> 
+
+                <View 
+                    style = {{
+                        flex: 1, 
+                        padding: SIZES.medium
+                    }}
+                > 
+                    <Welcome 
+                    
+                    /> 
+
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 export default Home;    
