0bc448a2bcd54f9789d875a6f9a99045fd7a884a
Grampachampa <grampachampa@gmail.com>
2023-05-15 15:54:26 +0200

    swag

--- app/upload.js
+++ app/upload.js
@@ -4,8 +4,12 @@ import { COLORS, icons, images, SIZES } from '../constants';
 
 export default outputPage = () => {
   return (
-      <SafeAreaView style = {{fled: 1, backgroundColor: COLORS.backgroundBlue}}>
-          <View style={{alignItems: 'center'}}><Text style = {{color: COLORS.lighterGreen}}>Upload Page</Text></View>           
+      <SafeAreaView style = {{fled: 1, backgroundColor: COLORS.backgroundBlue}} >
+          <View style={{alignItems: 'center', backgroundColor:COLORS.backgroundBlue}}>
+            <Text style = {{color: COLORS.white, fontWeight: "bold", fontSize: "60px"}}>Upload Page</Text>
+            <Text style = {{color: COLORS.lighterGreen, fontWeight: "bold", fontSize: "20px", textAlign: "center", position: "relative", marginTop:"50%"}}>This Page is not ready for the Beta, please check in later!</Text>
+            <Text style = {{color: COLORS.backgroundBlue, marginTop:"50000%"}}>Sneaky Text</Text>
+            </View>           
       </SafeAreaView>
   )
 }
